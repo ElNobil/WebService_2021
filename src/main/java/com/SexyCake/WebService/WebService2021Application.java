6f6919207541c43f49d2bc507fbc2219b0b21883
Nobil <elnobil2001@gmail.com>
2021-02-15 12:27:19 -0300

    Project created

---
+++ src/main/java/com/SexyCake/WebService/WebService2021Application.java
@@ -0,0 +1,13 @@
+package com.SexyCake.WebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebService2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebService2021Application.class, args);
+	}
+
+}
